3d728263ee2c4002170020814aaf40b07a8c4e31
slovo_cheburashki <m9056883999@gmail.com>
2026-02-13 17:33:13 +0300

    Initial commit

---
+++ src/main/java/com/masha/studyplannerapi/StudyPlannerApiApplication.java
@@ -0,0 +1,13 @@
+package com.masha.studyplannerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPlannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPlannerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masha/studyplannerapi/StudyPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masha.studyplannerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlannerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
